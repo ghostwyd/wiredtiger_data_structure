4a11b5ea4bf4edc0f13c7ecc0b0fde17c2f71d93
wuyongdong <wuyongdong@bytedance.com>
2022-12-27 16:44:25 +0800

    feat: add more digraph

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
 dot -Tsvg page.dot > page.svg
+dot -Tsvg data_handle.dot > data_handle.svg
+dot -Tsvg btree.dot > btree.svg
+dot -Tsvg txn.dot > txn.svg
+dot -Tsvg cache.dot > cache.svg
+dot -Tsvg checkpoint.dot > checkpoint.svg
