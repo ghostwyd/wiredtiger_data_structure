7ea2c6c96ad4903c7e0a5d7ff91908ccad5c74ef
wuyongdong <wuyongdong@bytedance.com>
2023-01-03 23:07:09 +0800

    feat: 更新threa/evict和page

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@ dot -Tsvg page.dot > page.svg
 dot -Tsvg data_handle.dot > data_handle.svg
 dot -Tsvg btree.dot > btree.svg
 dot -Tsvg txn.dot > txn.svg
+dot -Tsvg thread.dot > thread.svg
 dot -Tsvg cache.dot > cache.svg
 dot -Tsvg checkpoint.dot > checkpoint.svg
+dot -Tsvg evict.dot > evict.svg
